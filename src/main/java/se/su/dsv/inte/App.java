8fbaffba05f958eca47f7e4fb4bb452172e9f568
Axel Andersson <xaz0g@hotmail.com>
2017-09-28 15:13:31 +0200

    First commit

---
+++ src/main/java/se/su/dsv/inte/App.java
@@ -0,0 +1,13 @@
+package se.su.dsv.inte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
