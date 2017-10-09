ee185d1c65cecd34e27691d2e68e0da62fc77553
Jendelheim <victor.jendelheim@hotmail.com>
2017-10-09 15:01:21 +0200

    Pushing and commiting second try

--- src/main/java/se/su/dsv/inte/Johan.java
+++ src/main/java/se/su/dsv/inte/Johan.java
@@ -3,4 +3,5 @@ package se.su.dsv.inte;
 public class Johan {
 	String Johan = "Johan är fortfarande bäst!";
 	String Victor = "Men victor är trots allt lite bättre ändå!";
+	String Marie = "Hon trodde hon var ännu bättre, men hon är ju ändå skåning.. ";
 }
