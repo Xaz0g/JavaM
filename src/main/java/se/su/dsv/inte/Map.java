845c4d7fe16959031b3918380c77984c317730a3
Axel Andersson <xaz0g@hotmail.com>
2017-10-09 16:46:45 +0200

    Map test design

--- src/main/java/se/su/dsv/inte/Map.java
+++ src/main/java/se/su/dsv/inte/Map.java
@@ -6,13 +6,13 @@ public interface Map {
 	
 	public int getWidth();
 	
-	public Tile getTile();
+	public Tile getTile(int row, int column);
 	
 	public void setTile(Tile tile, int row, int column);
 	
-	public MapObject getPlayerCharacter();
+	public PlayerCharacter getPlayerCharacter();
 	
-	public void placeMapObject(MapObject mObject, int row, int column);
+	public void placeMapObject(PlayerCharacter mObject, int row, int column);
 	
 	public void moveMapObject(int fromRow, int fromColumn, int toRow, int toColumn);
 
