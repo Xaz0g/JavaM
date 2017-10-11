7974e7760288933887b7c2cb22962c6821b526ee
Axel Andersson <xaz0g@hotmail.com>
2017-10-11 13:38:27 +0200

    Första testet PlayChar

--- src/main/java/se/su/dsv/inte/PlayerCharacter.java
+++ src/main/java/se/su/dsv/inte/PlayerCharacter.java
@@ -2,4 +2,32 @@ package se.su.dsv.inte;
 
 public class PlayerCharacter {
 	
+	private String name;
+	private double maxHealth, currentHealth;
+	private int speed;
+	
+	public PlayerCharacter(String name, double startingHealth, int startingSpeed) {
+		this.name = name;
+		this.maxHealth = startingHealth;
+		this.currentHealth = this.maxHealth;
+		this.speed = startingSpeed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getMaxHealth() {
+		return maxHealth;
+	}
+
+	public double getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+	
+	
 }
