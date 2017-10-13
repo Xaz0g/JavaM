935d784197ab17072099f750bc84c523071857f8
Johan <Johan.Eklundh@gmail.com>
2017-10-13 17:21:02 +0200

    ContainerObjects + Enemys konstuktor + skapat enemyTest

--- src/main/java/se/su/dsv/inte/ContainerObject.java
+++ src/main/java/se/su/dsv/inte/ContainerObject.java
@@ -2,6 +2,10 @@ package se.su.dsv.inte;
 
 public class ContainerObject extends MapObject {
 	
+	Item[] container;
 	
+	public ContainerObject(int i) {
+		container = new Item[i];
+	}
 	
 }
--- src/main/java/se/su/dsv/inte/Enemy.java
+++ src/main/java/se/su/dsv/inte/Enemy.java
@@ -4,8 +4,10 @@ public class Enemy extends MapObject{
 	String name;
 	private double maxHealth, currentHealth;
 	private int speed;
+	private final int standardHealth = 100; 
 	
 	public Enemy(String name){
 		this.name = name;
+		maxHealth = currentHealth = standardHealth;
 	}
 }
