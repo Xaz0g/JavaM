ab84c54598d2c4629eb12db6ea1b846baa9e9a2f
Jendelheim <victor.jendelheim@hotmail.com>
2017-10-16 14:52:22 +0200

    Tagit bort alla importerade metoder i subklasserna till Item

--- src/main/java/se/su/dsv/inte/Weapon.java
+++ src/main/java/se/su/dsv/inte/Weapon.java
@@ -8,33 +8,4 @@ public class Weapon extends Item {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public int getWeight() {
-
-	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double getVelocity() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getAttack() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getArmorValue() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
