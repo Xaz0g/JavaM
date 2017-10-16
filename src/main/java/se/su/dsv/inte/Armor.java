48e3c72108406d21e4d95ee4489228d9873739c5
marietopphem <marietopphem@gmail.com>
2017-10-16 14:47:18 +0200

    Tagit bort alla getMetoder från Weapon, Cosmetics ad Armor

--- src/main/java/se/su/dsv/inte/Armor.java
+++ src/main/java/se/su/dsv/inte/Armor.java
@@ -7,34 +7,4 @@ public class Armor extends Item{
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public int getWeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double getVelocity() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getAttack() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getArmorValue() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
