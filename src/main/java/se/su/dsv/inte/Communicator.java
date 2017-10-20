f70da445336a4c61ef17c8955aaef2628eda6a26
Jendelheim <victor.jendelheim@hotmail.com>
2017-10-20 10:26:45 +0200

    Added methods to: Communicator.class

--- src/main/java/se/su/dsv/inte/Communicator.java
+++ src/main/java/se/su/dsv/inte/Communicator.java
@@ -2,17 +2,17 @@ package se.su.dsv.inte;
 
 public class Communicator {
 
-    // Movement
+    // Strings
 
-    public void successfulMovement(int fromRow, int fromColumn, int toRow, int toColumn){
-        System.out.println("You have successfully moved character from [" + fromRow + "," + fromColumn + "] to [" + toRow + "," + toColumn + "].");
+    public String successfulMovement(int fromRow, int fromColumn, int toRow, int toColumn){
+        return ("You have successfully moved character from [" + fromRow + "," + fromColumn + "] to [" + toRow + "," + toColumn + "].");
     }
 
-    public void unsuccessfulMovement(int fromRow, int fromColumn, int toRow, int toColumn) {
-        System.out.println("It is not possible to move character from [" + fromRow + "," + fromColumn + "] to [" + toRow + "," + toColumn + "].");
+    public String unsuccessfulMovement(int fromRow, int fromColumn, int toRow, int toColumn) {
+        return ("It is not possible to move character from [" + fromRow + "," + fromColumn + "] to [" + toRow + "," + toColumn + "].");
     }
 
-    public void movementCheck(boolean status){
+    public String  movementCheck(boolean status){
         String statusString;
         if(status) {
             statusString = "";
@@ -21,41 +21,42 @@ public class Communicator {
             statusString = "not ";
         }
 
-        System.out.println("It is " + statusString + "possible to move to this position!");
+        return ("It is " + statusString + "possible to move to this position!");
     }
 
-
-
-    // Item & inventory
-
-    public void receivedItem(Item item){
+    public String receivedItem(Item item){
         String itemName = item.getName();
-        System.out.println(itemName + " has been added to inventory!");
+        return (itemName + " has been added to inventory!");
     }
 
-    public void deletedItem(Item item){
+    public String deletedItem(Item item){
         String itemName = item.getName();
-        System.out.println(itemName + " has been deleted from inventory!");
+        return (itemName + " has been deleted from inventory!");
     }
 
-    // Battle
-
-    public void damageDealt(EntityObject entObj, EntityObject enemy){
-        System.out.println(entObj.getName() + " did " + entObj.getDamage() + " damage to " + enemy.getName() + "!");
+    public String damageDealt(EntityObject entObj, EntityObject enemy){
+        return(entObj.getName() + " did " + entObj.getDamage() + " damage to " + enemy.getName() + "!");
     }
 
-    public void damageAbsorbed(EntityObject entObj, EntityObject enemy){
+    public String damageAbsorbed(EntityObject entObj, EntityObject enemy){
         double reducedBase = 1.0 * enemy.getArmor();
         double actualDamage = entObj.getDamage() * reducedBase;
         double absorbedDamage = entObj.getDamage() - actualDamage;
 
-        System.out.println(enemy.getName() + " just absorbed " + absorbedDamage + " (" + enemy.getArmor() + ").");
+        return(enemy.getName() + " just absorbed " + absorbedDamage + " (" + enemy.getArmor() + ").");
     }
 
+    public String youveDied(){
+        return("You've died.");
+    }
 
+    public String youveBeenKilledBy(EntityObject enemy){
+        return(enemy.getName() + " killed you.");
+    }
 
-    //
-
+    public String youveKilledEnemy(EntityObject enemy){
+        return("You've killed " + enemy.getName());
+    }
 
 
 
