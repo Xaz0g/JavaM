8321dd27042a582697a31416be0d2d516cd4c3cb
Axel Andersson <xaz0g@hotmail.com>
2017-10-24 12:36:29 +0200

    Added code to MOC

--- src/test/java/se/su/dsv/inte/MapObjectControllerTest.java
+++ src/test/java/se/su/dsv/inte/MapObjectControllerTest.java
@@ -3,6 +3,7 @@ package se.su.dsv.inte;
 import static org.junit.Assert.*;
 import org.junit.*;
 import se.su.dsv.inte.map.Map;
+import se.su.dsv.inte.map.Terrain;
 import se.su.dsv.inte.map.objects.MapObject;
 
 public class MapObjectControllerTest {
@@ -10,8 +11,14 @@ public class MapObjectControllerTest {
 	private Map testMap = new Map(10,10);
 	private MapObject o = new MapObject(testMap,"testObject",10);
 	
+	{
+		testMap.setMapTiles(Terrain.Grass);
+		testMap.placeMapObject(o, 5, 5);
+	}
+	
 	@Test
 	public void testDestroyObject() {
-		
+		assertTrue(testMap.getTile(5, 5).isOccupied());
+		o.modifyCurrentHealth(10);
 	}
 }
